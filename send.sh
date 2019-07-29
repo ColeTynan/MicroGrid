fe469f4d83a64efbc0471be2f83f13c5ba6cfd3a
Jimmy Qiu <jiamingqiu951@gmail.com>
2019-07-29 16:16:52 -0700

    new name for file

--- send.sh
+++ send.sh
@@ -4,7 +4,7 @@ file=$1
 IPs='/mnt/c/Users/jiami/Desktop/VSCode_srip/ip_list.txt'
 echo "Starting..."
 while read line; do
-	command="scp ${file} pi@${line}:~"
+	command="scp ${file} pi@${line}:~/sync/"
 	echo $command
 	eval $command
 done < "$IPs"
