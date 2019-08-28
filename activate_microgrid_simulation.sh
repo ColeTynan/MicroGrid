759351f699bf682718c3ca3c1bc4ecd2bb5ed359
Cole Tynan <ctynanwo@ucsd.edu>
2019-08-28 12:44:53 -0700

    Created two-hop version, no difference yet from single hop

--- activate_microgrid_simulation.sh
+++ activate_microgrid_simulation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#activateRunRatCon.sh
-#use this to run the script that runs the ratio consensus program on each pi, which sends all output to a remote machine to compile and format the data acquired
+#activate_microgrid_simulation.sh
+#use this to run a simulation on each pi, which sends all output to a remote machine to compile and format the data acquired
 #USAGE: ./path/activate graph_structure(integer value from 0-7) reference_file(int value) IP_File.txt(int value) [leave as 0 for defaults]
 #Alternate usage: Run without parameters and you will be prompted for them
 #	Graph structure options (figures in accompanying document)
@@ -24,10 +24,10 @@ FORMAT_REMINDER='USAGE: ./path/activate graph_structure(integer value from 0-7) 
 SCRIPT='runratcon.sh'
 GRAPHS=('graphs/ring4.txt' 'graphs/ring8.txt' 'graphs/smallASym1.txt' 'graphs/smallASym2.txt' 'graphs/hook.txt' 'graphs/treeBal7.txt' 'graphs/treeUnb7.txt')
 IP_FILES=('IPs/fourIP.txt' 'IPs/eightIP.txt')
-REF_FILES=('reg-d-abridged.CSV' 'reg-d.CSV' 'reg-d-factor5.CSV')
+REF_FILES=('reg-d-abridged.CSV' 'reg-d.CSV' 'reg-d-factor1000.CSV')
 SIM_FILES=('runratcon.sh' 'runsadpoint.sh')
 DEF_REF=0
-DEF_IP=0
+DEF_IP=1
 DEF_GRAPH=0
 DEF_SIM=0
 SIM_I=$1
@@ -56,13 +56,10 @@ fi
 #prompt for graph structure
 if [[ -z "$GRAPH_I" ]]; then
 	echo "Which graph structure would you like to test?"
-	echo "Graph structure options (See accompanying PDF for illustrations):
-	 1. ring4.txt						-- ring of four agents
-	 2. ring8.tx						-- ring of eight agents
-	 4. smallASym.txt				-- small asymmetrical structure of four agents
-	 5. hook.txt						-- shaped like a "hook" or a linked list
-	 6. treeBal7.txt				-- a balanced binary tree strcture
-	 7. treeUnb7.txt				-- unbalanced binary tree "
+	for ((i = 0; i < "${#GRAPHS[@]}"; i++)); do
+		index="$((i+1))"
+		echo -e "\t"$index". ${GRAPHS["$i"]}"
+	done
 	read -n 1 GRAPH_I
 	echo ""
 fi 
@@ -77,9 +74,9 @@ fi
 if [[ -z "$REF_I" ]]; then
 	echo "Which reference signal data file would you like to use?"
 	echo "Reference file options:
-	 1. reg-d-abridged.CSV	-- the first ten values from reg-d.
-	 2. reg-d.CSV						-- The full reg-d file, containing 1200 entries. Takes ~40 minutes. 
-	 3. reg-d-factor5.CSV		-- reg-d but every value is increased by a factor of 5 (TODO, unavailable)"
+	 1. reg-d-abridged.CSV			-- the first ten values from reg-d for debugging purposes
+	 2. reg-d.CSV								-- The full reg-d file, containing 1200 entries. Takes ~40 minutes. 
+	 3. reg-d-factor1000.CSV		-- reg-d but every value is increased by a factor of 5 (TODO, unavailable)"
 	read -n 1 REF_I
 	echo ""
 fi
@@ -92,6 +89,8 @@ fi
 
 #Getting input for IP file (TODO: provide a workaround for this. Usually running on all pis doesnt cause problems)
 if [[ -z "$IP_I" ]]; then
+
+: << 'END'
 	echo "IP file options: (will remove this option later, not necessary)
 	 1. First four Pis
 	 2. first eight pis
@@ -99,10 +98,11 @@ if [[ -z "$IP_I" ]]; then
 	 read -n 1 IP_I
 	 echo ""
 fi 
+END
 
-if [[ "$IP_I" -gt 0 ]]; then
-	IP_I="$((IP_I - 1))"
-else
+#if [[ "$IP_I" -gt 0 ]]; then
+#	IP_I="$((IP_I - 1))"
+#else
 	IP_I="$DEF_IP"
 fi
 
